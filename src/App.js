fae5e95d96d926911a7c5f0eb705e8ca5ffe94ac
Artem Kulikov - Work <a.v.kulikov@novotelecom.ru>
2020-02-07 12:20:16 +0700

    [+] Добавил react-router-dom. Настройка роутинга.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import React from 'react';
+import {BrowserRouter, Switch, Route} from 'react-router-dom'
 import {Navbar} from './component/Navbar';
 import {Home} from './pages/Home';
+import {Profile} from './pages/Profile';
+import {About} from './pages/About';
 
 function App() {
   return (
-    <React.Fragment>
+    <BrowserRouter>
       <Navbar />
       <div className="container pt-4">
-        <Home />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/about" exact component={About} />
+          <Route path="/profile/:name" exact component={Profile} />
+        </Switch>
       </div>
-    </React.Fragment>
+    </BrowserRouter>
   );
 }
 
