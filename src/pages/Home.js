29550407b1a6ead057d849d61a71df30ee4a4217
Artem Kulikov - Work <a.v.kulikov@novotelecom.ru>
2020-02-10 17:54:23 +0700

    [+] Add components: Card, Search. Change Home Page.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,23 @@
 import React from 'react'
+import {Search} from '../component/Search'
+import {Card} from '../component/Card'
 
 export const Home = () => {
+    const cards = new Array(15)
+        .fill('')
+        .map((_, i) => i)
     return (
-        <div>
-            <h1>Home page</h1>
-        </div>
+        <React.Fragment>
+            <Search />
+            <div className="row">
+                {cards.map(card => (
+                    <div className="col-sm-4 mb-4" key={card}>
+                        <Card />
+                    </div>
+                ))}
+
+            </div>
+
+        </React.Fragment>
     )
 }
